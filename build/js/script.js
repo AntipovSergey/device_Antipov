2518e5a926efac3b80614b23ac100d67fed09510
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-09-20 14:14:47 +0300

    Завершает верстку, стилизацию и адаптацию блока questions

--- build/js/script.js
+++ build/js/script.js
@@ -1,35 +1,37 @@
-// //Маска для поля tel
-// let selector = document.getElementById('form-tel');
-// let im = new Inputmask('+7 (999) 999-99-99');
+//Маска для поля tel
+let selector = document.getElementById('tel');
+let im = new Inputmask('+7 (999) 999-99-99');
 
-// if(selector) {
-//   im.mask(selector);
-// }
+if(selector) {
+  im.mask(selector);
+}
 
-// //Отправка формы
-// const form = document.querySelector('.promo__form');
-// if(form) {
-//   form.onsubmit = async (e) => {
-//     e.preventDefault();
+//Отправка формы
+const form = document.querySelector('.questions__form');
+if(form) {
+  form.onsubmit = async (e) => {
+    e.preventDefault();
 
-//     await fetch('https://echo.htmlacademy.ru/', {
-//       method: 'POST',
-//       body: new FormData(form)
-//     })
+    await fetch('https://echo.htmlacademy.ru/', {
+      method: 'POST',
+      body: new FormData(form)
+    })
 
-//     form.reset();
-//   };
-// }
+    form.reset();
+  };
+}
 
-// //Local storage
-// window.addEventListener('DOMContentLoaded', function(){
-//   const formTel = document.getElementById('form-tel');
-//   const formName = document.getElementById('form-name');
-//   const button = document.querySelector('.form__button')
-//   if(button) {
-//     button.addEventListener('click', function(){
-//         localStorage.setItem('tel', formTel.value);
-//         localStorage.setItem('name', formName.value);
-//     })
-//   }
-// })
+//Local storage
+window.addEventListener('DOMContentLoaded', function(){
+  const formTel = document.getElementById('tel');
+  const formName = document.getElementById('name');
+  const formText = document.getElementById('text');
+  const button = document.querySelector('.form__button')
+  if(button) {
+    button.addEventListener('click', function(){
+        localStorage.setItem('tel', formTel.value);
+        localStorage.setItem('name', formName.value);
+        localStorage.setItem('text', formText.value);
+    })
+  }
+})
